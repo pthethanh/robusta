de671f42b36babc5e0a63a9be2090d28e340741c
pthethanh <pthethanh@tma.com.vn>
2019-03-14 22:46:50 +0700

    admin template copy from github

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,11 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header height="45px">
-        <app-header></app-header>
-      </el-header>
-      <el-main>
-        <router-view></router-view>
-      </el-main>
-    </el-container>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
 export default {
-  name: 'app',
-  components: {
-    'app-header': Header
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-</style>
--- web/src/main.js
+++ web/src/main.js
@@ -1,13 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store/index'
+import router from './router/index'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+import axios from 'axios'
 import './registerServiceWorker'
 import './plugins/element.js'
-import axios from 'axios'
+
+import '../mock' // simulation data
+
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
-
 // Set base URL to backend API service
 const backendAddr = process.env.BIRD_BACKEND_ADDRS || 'http://localhost:8080'
 console.log(`BIRD_BACKEND_ADDRS: ${backendAddr}`)
@@ -15,7 +28,8 @@ axios.defaults.baseURL = backendAddr
 Vue.prototype.$http = axios
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
