98631a4ea4570149e694a2f48fb70d0f1dd43441
thanhpham <phamthethanh1088@gmail.com>
2019-09-18 20:07:26 +0700

    [playground] scrollable menu - temporary

--- web/src/views/playground/index.vue
+++ web/src/views/playground/index.vue
@@ -3,17 +3,19 @@
     <el-row type="flex" justify="center">
       <el-col :span="4" class="left">
         <el-menu default-active="1">
-          <el-menu-item v-for="challenge in  challenges" :key="challenge.id" @click="onClick(challenge)">{{challenge.title}}</el-menu-item>
+          <el-menu-item v-for="(challenge,index) in  challenges" :key="challenge.id" @click="onClick(challenge)">{{index + 1 + '. ' + challenge.title}}</el-menu-item>
         </el-menu>
       </el-col>
-      <el-col :span="20" class="right">
-        <div class="description" v-if="selected !== null">
-          <div class="title">{{selected.title}}</div>
-          <div class="content">
-            {{selected.description}}
+      <el-col :span="20">
+        <div class="right">
+          <div class="description" v-if="selected !== null">
+            <div class="title">{{selected.title}}</div>
+            <div class="content">
+              {{selected.description}}
+            </div>
           </div>
+          <challenge-player v-if="selected !== null" :code="selected.sample" :challenge_id="selected.id" class="editor"></challenge-player>
         </div>
-        <challenge-player v-if="selected !== null" :code="selected.sample" :challenge_id="selected.id" class="editor"></challenge-player>
       </el-col>
     </el-row>
   </div>
@@ -56,34 +58,19 @@ export default {
 
 <style lang="scss" scoped>
 .playground {
-  height: 100%;
   line-height: 1.5em;
 
   .left {
-    height: 100%;
     border: 1px solid lightgrey;
+    min-width: 200px;
+    overflow-x: scroll;
   }
 
   .right {
-    padding-top: 5px;
+    position: fixed;
+    top: 10px;
     height: 100%;
-
-    .course-info {
-      .title {
-        font-size: 2.5em;
-        font-family: 'Open Sans', sans-serif;
-        word-break: normal;
-        font-weight: 700;
-      }
-
-      .introduction {
-        font-family: "Merriweather", serif;
-        letter-spacing: 0.01rem;
-        font-size: 0.95rem;
-        line-height: 1.75em;
-        color: rgba(0, 0, 0, 0.84);
-      }
-    }
+    min-width: 70%;
 
     .description {
       padding: 10px 25px 20px 25px;
