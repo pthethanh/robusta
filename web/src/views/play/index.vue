b570eb8812227e37b18da124dfbb8789cc89302f
pthethanh <pthethanh@TMA.COM.VN>
2019-10-14 16:47:33 +0700

    [ui] dragable list - init

--- web/src/views/play/index.vue
+++ web/src/views/play/index.vue
@@ -1,70 +1,48 @@
 <template>
-  <div>
-    <el-row type="flex" justify="center">
-      <el-col :xs="24" :sm="24" :md="18" :lg="14" :xl="14">
-        <infinity-load :fetch-data="fetchData" v-bind:data.sync="data" @error="offset = offset - limit">
-          <template v-slot:default="{data}">
-            <div v-for="(item, index) in filterData" :key="index">
-              <div>
-                {{item.title}} -- {{item.created_by_name}}
-              </div>
-            </div>
-          </template>
-        </infinity-load>
-        <el-button @click="logData">Log Data</el-button>
-      </el-col>
-    </el-row>
-  </div>
-
+  <el-row type="flex" justify="center">
+    <el-col :span="14">
+      <dnd-list :list1="list1" :list2="list2" list1-title="Selected" list2-title="Available">
+        <template v-slot:list1="{data}">
+          <div class="list-complete-item-handle">
+            {{ data.title }}
+          </div>
+        </template>
+        <template v-slot:list2="{data}">
+          <div class="list-complete-item-handle2">
+            {{ data.title }}
+          </div>
+        </template>
+      </dnd-list>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-import InfinityLoad from '@/components/InfinityLoad'
+import DndList from '@/components/DndList'
 import {
   fetchList
 } from '@/api/article'
 export default {
+  name: 'DndListDemo',
   components: {
-    InfinityLoad
+    DndList
   },
   data () {
     return {
-      data: [],
-      offset: -10,
-      limit: 10,
-      tags: []
+      list1: [],
+      list2: []
     }
   },
-  computed: {
-    filterData() {
-      var rs = []
-      for (var i = 0; i < this.data.length; i++) {
-        if (this.tags.length === 0) {
-          rs.push(this.data[i])
-          continue
-        }
-        for (var j = 0; j < this.tags.length; j++) {
-          if (this.data[i].tags.includes(this.tags[j])) {
-            rs.push(this.data[i])
-            break
-          }
-        }
-      }
-      return rs
-    }
+  created () {
+    this.getData()
   },
   methods: {
-    fetchData () {
-      this.offset += this.limit
-      return fetchList('offset=' + this.offset + '&limit=' + this.limit)
-    },
-    logData () {
-      console.log(this.data)
-      if (this.tags.length === 0) {
-        this.tags.push('interview preparation')
-        return
-      }
-      this.tags = []
+    getData () {
+      this.listLoading = true
+      fetchList('').then(response => {
+        this.list2 = response.data.splice(0, 5)
+        this.list1 = response.data
+      })
     }
   }
 }
